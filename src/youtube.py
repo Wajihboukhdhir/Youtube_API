7f971d8295b75e8930ef26dda75a9792e590ddc9
Wajihboukhdhir <83852296+Wajihboukhdhir@users.noreply.github.com>
2022-01-22 21:24:31 +0100

    Update youtube.py

--- src/youtube.py
+++ src/youtube.py
@@ -210,7 +210,7 @@ def statistics():
 
 
 @youtubes.get("/singers")
-@swag_from('youtube_api/src/docs/auth/Singers.yaml')
+@swag_from('C:/Users/Leila/Desktop/bookmarksrestapi/src/docs/auth/Singers.yaml')
 
 
 @jwt_required()
